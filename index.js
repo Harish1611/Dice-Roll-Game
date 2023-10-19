db6450bb1429838c8ed79488206666ad0de2cc1f
Harish1611 <harishnaidub005@gmail.com>
2023-10-19 11:01:30 +0530

    Modified the Folder Structure

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.round((Math.random()*5)+1);
+var randomNumber2= Math.round((Math.random()*5)+1);
+
+var ranImg = 'images/dice'+randomNumber1+'.png'
+var ranImg2 = 'images/dice'+randomNumber2+'.png'
+
+
+document.querySelector('.img1').setAttribute('src',ranImg);
+document.querySelector('.img2').setAttribute('src',ranImg2);
+
+function rollDice(){
+
+  location.reload();
+}
+if(randomNumber1 > randomNumber2){
+  document.querySelector('h1').innerHTML = "🏴󠁧󠁢󠁥 Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector('h1').innerHTML = "Player 2 Wins! 🏴󠁧󠁢󠁥";
+}
+else if(randomNumber1 === randomNumber2){
+  document.querySelector('h1').innerHTML = "Draw";
+}
